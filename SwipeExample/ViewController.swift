50f2c3485b1d8b962b77832b1866e3c19502f564
Prethush <prethush.pj@experionglobal.com>
2017-03-22 13:01:09 +0530

    Initial Commit

---
+++ SwipeExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwipeExample
+//
+//  Created by Prethush on 22/03/17.
+//  Copyright © 2017 Experion Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
